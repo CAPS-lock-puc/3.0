55d80b3af405fc44bce01829a1b0bb9c8629cb19
JoaoVictorBalvedi <joaovictorbalvedi@gmail.com>
2024-11-18 23:27:41 -0300

    animais

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from models.iot.devices import Device
 from models.iot.sensors import Sensor
 from models.iot.actuators import Atuador
 from models.iot.historico import Historico
+from models.iot.animal import Animal
